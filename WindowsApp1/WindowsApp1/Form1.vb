0436154ee44bdbb68360b7fe626b1de3ed842bcd
jacobass <5511068t@gmail.com>
2018-08-17 00:29:48 +0900

    20180817

---
+++ WindowsApp1/WindowsApp1/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnMessage.Click
+
+    End Sub
+End Class
