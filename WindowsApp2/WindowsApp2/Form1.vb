fe7e0030b136656a4d659bcd1551f9ac7e86c72d
jacobass <5511068t@gmail.com>
2018-08-26 21:57:30 +0900

    20180826

---
+++ WindowsApp2/WindowsApp2/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub ShowMessage(sender As Object, e As EventArgs) Handles btnMessage.Click
+        lblMessage.Text = "Hello VB!"
+    End Sub
+
+    Private Sub ExitProc(sender As Object, e As EventArgs) Handles btnExit.Click
+        Application.Exit()
+    End Sub
+End Class
